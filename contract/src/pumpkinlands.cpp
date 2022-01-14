25f1d49dac28a3b93d2b97ea5145ce063acfbf3a
Benjamin Wheeler <benjiewheeler@yandex.com>
2022-01-14 15:13:18 +0000

    chore: initiated the contract setup

---
+++ contract/src/pumpkinlands.cpp
@@ -0,0 +1,5 @@
+#include <pumpkinlands.hpp>
+ACTION pumpkinlands::hi( name nm ) {
+   /* fill in action body */
+   print_f("Name : %\n",nm);
+}
---
+++ scripts/setup.sh
@@ -0,0 +1,15 @@
+if [ ! $(which eosio-cpp) ]; then
+    wget -nc https://github.com/EOSIO/eos/releases/download/v2.0.13/eosio_2.0.13-1-ubuntu-18.04_amd64.deb
+    wget -nc https://github.com/EOSIO/eosio.cdt/releases/download/v1.7.0/eosio.cdt_1.7.0-1-ubuntu-18.04_amd64.deb
+    apt update
+    apt install -y ./eosio_2.0.13-1-ubuntu-18.04_amd64.deb
+    apt install -y ./eosio.cdt_1.7.0-1-ubuntu-18.04_amd64.deb
+    apt install -y clang-format-10
+    rm -rf ./eosio_2.0.13-1-ubuntu-18.04_amd64.deb
+    rm -rf ./eosio.cdt_1.7.0-1-ubuntu-18.04_amd64.deb
+fi;
+
+if [ ! -d contract ]; then
+	eosio-init --project pumpkinlands
+	mv pumpkinlands contract
+fi;
